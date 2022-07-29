7057eb3e077b79a2b272d6c3d03b30f2024dcb7b
jiwoo <sgo8308@gmail.com>
2022-07-29 09:45:40 +0900

    commit

---
+++ src/main/java/handera/githubactionspractice/GithubActionsPracticeApplication.java
@@ -0,0 +1,13 @@
+package handera.githubactionspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handera/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package handera.githubactionspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
