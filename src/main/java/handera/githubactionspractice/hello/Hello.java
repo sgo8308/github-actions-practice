06fb879e23ee1c4a511f718c9e558e74ad7b16b8
jiwoo <sgo8308@gmail.com>
2022-07-29 14:05:48 +0900

    jacoco

---
+++ src/main/java/handera/githubactionspractice/hello/Hello.java
@@ -0,0 +1,12 @@
+package handera.githubactionspractice.hello;
+
+public class Hello {
+
+    void hello(boolean bool) {
+        if (bool) {
+            System.out.println("this is true");
+        }
+
+        System.out.println("this is false");
+    }
+}
