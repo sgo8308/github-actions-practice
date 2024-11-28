7e2c6e8dfb556bc30fbbc620d1f01a744af4a29d
ftwz-shef <146905128+ftwz-shef@users.noreply.github.com>
2024-11-28 11:24:05 +0900

    test 실패시 결과물 나오게

--- src/test/java/handera/githubactionspractice/GithubActionsPracticeApplicationTests.java
+++ src/test/java/handera/githubactionspractice/GithubActionsPracticeApplicationTests.java
@@ -3,12 +3,16 @@ package handera.githubactionspractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GithubActionsPracticeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void successtest() {
 		System.out.println("hi");
 	}
 
+  @Test
+  void failtest() {
+    System.out.println("hi");
+    throw new RuntimeException("fail");
+  }
 }
